1a1cffcf8530b83a7c019af0456164205303ba4d
Elena Swiderskaya <olenyonock7@rambler.ru>
2024-05-30 16:26:01 +0300

    Добавляет детализацию главной странице

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,8 @@
 /* в этот файл добавляет скрипты*/
+const menu = document.querySelector('.navigation__menu-button');
+const navigation = document.querySelector('.navigation__list');
+menu.addEventListener('click', () => {
+  menu.classList.toggle('navigation__menu-button--open');
+  navigation.classList.toggle('navigation__list--open');
+}
+);
