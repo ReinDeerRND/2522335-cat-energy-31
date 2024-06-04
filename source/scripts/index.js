924b85a47e4fe116febf1edf53ed97ac6852b41f
Elena Swiderskaya <olenyonock7@rambler.ru>
2024-06-04 11:06:28 +0300

    Добавляет оптимизацию верстки

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -6,6 +6,9 @@ header.classList.remove('header--no-js');
 const navigation = document.querySelector('.navigation__list');
 navigation.classList.remove('navigation__list--no-js');
 
+const map = document.querySelector('.map__interactive-map');
+map.classList.remove('map__interactive-map--no-js');
+
 // toggle-menu
 const menu = document.querySelector('.navigation__menu-button');
 menu.addEventListener('click', () => {
